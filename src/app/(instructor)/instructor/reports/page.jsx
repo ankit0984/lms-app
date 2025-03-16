cc5fabe562f92ed7b41ed025d31f0e6ce9e66ea1
ankit <ak1410mk@gmail.com>
2025-03-16 22:08:52 +0530

    instructor panel almost complete

---
+++ src/app/(instructor)/instructor/reports/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbList,
+    BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import ReportDashboard from "@/components/instructor/report/reports";
+
+export default function Page() {
+    return (
+        <SidebarInset>
+            <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
+                <div className='flex items-center gap-2 px-4'>
+                    <SidebarTrigger className='-ml-1' />
+                    <Separator
+                        orientation='vertical'
+                        className='mr-2 data-[orientation=vertical]:h-4'
+                    />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Study Material</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+            </header>
+            <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+                <div>
+                    <h1 className="text-2xl font-bold tracking-tight">Reports & Analytics</h1>
+                    <p className="text-muted-foreground">View detailed reports and analytics for your courses and students</p>
+                </div>
+                <ReportDashboard />
+            </div>
+        </SidebarInset>
+    );
+}
