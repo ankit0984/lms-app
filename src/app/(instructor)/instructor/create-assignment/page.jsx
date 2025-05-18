6d27ebe9b53eaf0b357bcfcb42be32ac55558f1c
ankit kumar <ak1410mk@gmail.com>
2025-05-19 01:46:42 +0530

    modified instructor panel

---
+++ src/app/(instructor)/instructor/create-assignment/page.jsx
@@ -0,0 +1,38 @@
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbList,
+	BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import CreateAssignments from "../../../../components/instructor/assignments/create-assignments";
+export const metadata = {
+	title: "Create-Assignments",
+	description: "Create assignments",
+};
+export default function Page() {
+	return (
+		<SidebarInset>
+			<header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
+				<div className='flex items-center gap-2 px-4'>
+					<SidebarTrigger className='-ml-1' />
+					<Separator
+						orientation='vertical'
+						className='mr-2 data-[orientation=vertical]:h-4'
+					/>
+					<Breadcrumb>
+						<BreadcrumbList>
+							<BreadcrumbItem>
+								<BreadcrumbPage>Assignments</BreadcrumbPage>
+							</BreadcrumbItem>
+						</BreadcrumbList>
+					</Breadcrumb>
+				</div>
+			</header>
+			<div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+				<CreateAssignments />
+			</div>
+		</SidebarInset>
+	);
+}
